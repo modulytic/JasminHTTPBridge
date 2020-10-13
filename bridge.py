971c1ee4bf784760035d9affd932de0b390560e1
Vidur Gupta <vidurgupta01@gmail.com>
2020-10-13 05:27:09 -0400

    Added round-robbin load balancing of endpoints

--- bridge.py
+++ bridge.py
@@ -17,6 +17,17 @@ conf_path = os.path.join(BRIDGE_PATH, "conf.json")
 with open(conf_path) as conf_f:
     conf_opts = json.load(conf_f)
 
+curr_endpoint = 0
+endpoint_path = os.path.join(BRIDGE_PATH, "curr_endpoint")
+with open(endpoint_path, 'r+') as endpoint_f:
+	curr_endpoint = int(endpoint_f.read())
+
+	endpoint_f.seek(0)
+	endpoint_f.write(str((curr_endpoint + 1) % len(conf_opts)))
+	endpoint_f.truncate()
+
+endpoint_f.close()
+
 # TODO: Add all supported params here: docs.jasminsms.com/en/latest/apis/ja-http/index.html#http-request-parameters
 values = {
     "to": routable.pdu.params['destination_addr'],
@@ -26,10 +37,10 @@ values = {
 http_data = urllib.urlencode(values)
 
 req = object()
-if conf_opts["method"] == "POST":
-    req = urllib2.Request(conf_opts["url"], http_data)
-elif conf_opts["method"] == "GET":
-    req = urllib2.Request(conf_opts["url"] + "?" + http_data)
+if conf_opts[curr_endpoint]["method"] == "POST":
+    req = urllib2.Request(conf_opts[curr_endpoint]["url"], http_data)
+elif conf_opts[curr_endpoint]["method"] == "GET":
+    req = urllib2.Request(conf_opts[curr_endpoint]["url"] + "?" + http_data)
 
 if req != None:
     urllib2.urlopen(req)
